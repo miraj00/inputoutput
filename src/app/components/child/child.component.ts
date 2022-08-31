67ec5fe8dea423687c874f0fa6cf370f2d2c3cea
miraj <miraj00@gmail.com>
2022-08-30 21:49:41 -0400

    @input added in child component to display parent component value

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Input() PData: number | undefined;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  public CData: number | undefined ;
   constructor() { }
 
   ngOnInit(): void {
