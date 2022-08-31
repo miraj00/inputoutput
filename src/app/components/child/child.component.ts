3699cf8651323dea024d15532f03a933fea0a0f2
miraj <miraj00@gmail.com>
2022-08-30 21:55:39 -0400

    event emitter added in child component

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,7 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   @Input() PData: number | undefined;
+  @Output() childEvent = new EventEmitter();
   constructor() { }
+
+  onChange(value: any) {
+    this.childEvent.emit(value);
+  }
 
   ngOnInit(): void {
   }
